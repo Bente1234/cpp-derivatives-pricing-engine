4075db002d36af279e62b8ec17af60ab0e4f4c6b
Bente1234 <b.liefhebber1@student.tue.nl>
2026-03-03 09:21:28 +0100

    Add CMake build and clean benchmark runner

---
+++ build/CMakeFiles/pricing_engine.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/20224246/OneDrive - TU Eindhoven/Documents/Vakantie Project 2025/Cpp-Derivatives-Pricing-Engine/src/binomial.cpp" "CMakeFiles/pricing_engine.dir/src/binomial.cpp.obj" "gcc" "CMakeFiles/pricing_engine.dir/src/binomial.cpp.obj.d"
+  "C:/Users/20224246/OneDrive - TU Eindhoven/Documents/Vakantie Project 2025/Cpp-Derivatives-Pricing-Engine/src/black_scholes.cpp" "CMakeFiles/pricing_engine.dir/src/black_scholes.cpp.obj" "gcc" "CMakeFiles/pricing_engine.dir/src/black_scholes.cpp.obj.d"
+  "C:/Users/20224246/OneDrive - TU Eindhoven/Documents/Vakantie Project 2025/Cpp-Derivatives-Pricing-Engine/src/main.cpp" "CMakeFiles/pricing_engine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/pricing_engine.dir/src/main.cpp.obj.d"
+  "C:/Users/20224246/OneDrive - TU Eindhoven/Documents/Vakantie Project 2025/Cpp-Derivatives-Pricing-Engine/src/monte_carlo.cpp" "CMakeFiles/pricing_engine.dir/src/monte_carlo.cpp.obj" "gcc" "CMakeFiles/pricing_engine.dir/src/monte_carlo.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/pricing_engine.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pricing_engine.dir/src/binomial.cpp.obj"
+  "CMakeFiles/pricing_engine.dir/src/binomial.cpp.obj.d"
+  "CMakeFiles/pricing_engine.dir/src/black_scholes.cpp.obj"
+  "CMakeFiles/pricing_engine.dir/src/black_scholes.cpp.obj.d"
+  "CMakeFiles/pricing_engine.dir/src/main.cpp.obj"
+  "CMakeFiles/pricing_engine.dir/src/main.cpp.obj.d"
+  "CMakeFiles/pricing_engine.dir/src/monte_carlo.cpp.obj"
+  "CMakeFiles/pricing_engine.dir/src/monte_carlo.cpp.obj.d"
+  "libpricing_engine.dll.a"
+  "pricing_engine.exe"
+  "pricing_engine.exe.manifest"
+  "pricing_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pricing_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
